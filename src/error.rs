743fbeaa3efab512b30eb4d98ad7fd6df101aa70
BrandonMurimi <Brandon26Charis@gmail.com>
2025-05-03 17:29:16 +0200

    Update error.rs

--- src/error.rs
+++ src/error.rs
@@ -1,1 +1,14 @@
+// src/error.rs
+#[derive(Debug)]
+pub enum ServiceError {
+    Validation(String),
+    BusinessRuleViolation(String),
+    NotFound,
+    RepositoryError(Box<dyn std::error::Error>),
+}
 
+impl From<Box<dyn std::error::Error>> for ServiceError {
+    fn from(err: Box<dyn std::error::Error>) -> Self {
+        ServiceError::RepositoryError(err)
+    }
+}
